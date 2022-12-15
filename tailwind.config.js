d6cdfebbb62a7588b31e1ccbcc740bfec763111c
liuxsdev <1028330144@qq.com>
2022-12-15 22:20:30 +0800

    add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
+    fontFamily: {
+      ubuntu: ["ubuntu", ...defaultTheme.fontFamily.sans],
+      din: ["din", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
+  darkMode: "class",
 };
