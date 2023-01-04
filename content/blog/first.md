3121aadcb2e14ab124005ccc6b27f7766cf050cb
liuxsdev <1028330144@qq.com>
2023-01-04 23:22:47 +0800

    add daisyui

--- content/blog/first.md
+++ content/blog/first.md
@@ -5,8 +5,3 @@ date = 2019-11-27
 
 This is my first blog post.
 
-# 标题 1
-
-## 标题 1.1
-
-# 标题 2
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,6 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   darkMode: "class",
 };
