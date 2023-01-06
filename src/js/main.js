c868e0101e3aea6306bdbbc449e23c00d672edde
liuxsdev <1028330144@qq.com>
2023-01-06 22:53:44 +0800

    feat:highlight toc

--- src/js/main.js
+++ src/js/main.js
@@ -35,12 +35,35 @@ function toogle_theme() {
 function getActiveTocElement() {
   const toc = document.querySelectorAll("h1");
   const current = [...toc].filter((item) => item.getBoundingClientRect().y < 0);
+  // console.log([...toc].map((item) => item.getBoundingClientRect().y));
+  // 取最后一个y小于0的元素,如果都不小于0,就返回第一个
+  if (current.length == 0) {
+    return toc[0];
+  }
   return current.reverse()[0];
+}
+
+function markToc() {
+  const activetoc = getActiveTocElement();
+  const id = activetoc.id;
+  toclist = document.querySelectorAll("#toc li a");
+  // 取消所有active
+  toclist.forEach((element) => {
+    element.dataset.active = false;
+  });
+  toclist_ids = [...toclist].map((item) => item.href.split("/#").reverse()[0]);
+  index = toclist_ids.indexOf(id);
+  current = toclist[index];
+  current.dataset.active = true;
 }
 
 window.onload = function () {
   const btn_toogle_theme = document.querySelector("#btn-toggle");
+  const main_dom = document.querySelector("div#main");
   btn_toogle_theme.onclick = toogle_theme;
+  main_dom.addEventListener("scroll", () => {
+    markToc();
+  });
 };
 
 document.addEventListener("DOMContentLoaded", function () {
